0e24c5d4da07fea526923800bb119df991e6822f
VinKE3 <harquin09@gmail.com>
2021-10-30 13:52:12 -0300

    commit1

---
+++ js/01-app.js
@@ -0,0 +1,12 @@
+//Desafío: Crear un algoritmo JS simple
+
+let usuario = prompt("Ingrese Nombre");
+let apellido= prompt("ingrese Apellido")
+let a = prompt("ingrese su numero de la suerte");
+let b = prompt("ingrese su edad");
+
+let suma = parseInt(a) + parseInt(b);
+
+// console.log("se va a morir a los: " + suma);
+
+console.log(`${usuario} ${apellido} se va a morir a los ${suma} años`);
---
+++ js/02-app.js
@@ -0,0 +1,25 @@
+//Desafío: crear un algoritmo con un condicional
+condicion = "Para ingresar a CompraGamer requiere dicha cantidad de dolares";
+alert(condicion);
+
+let nombre = prompt("ingrese su nombre");
+let presupuesto = prompt("ingrese presupuesto en dolares");
+
+if (presupuesto >= 300) {
+  mensaje = "Bienvenido a CompraGamer, continue con su registro";
+  let mail = prompt("Ingrese email");
+  let pass = prompt("ingrese contraseña");
+  let pass2 = prompt("Reingrese contraseña");
+
+  if (pass === pass2) {
+    mensaje2 = "Registro Correcto";
+  } else {
+    mensaje2 = "Contraseñas diferentes, reintente registro";
+  }
+
+  alert(mensaje2);
+} else {
+  mensaje = "Regrese cuando tenga mas dolares, gracias";
+}
+
+alert(mensaje);
